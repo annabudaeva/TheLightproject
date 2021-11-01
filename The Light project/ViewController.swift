0329f2dde1d079f1bdf83073c7c866d28ade721d
Anna <buanse@gmail.com>
2021-11-01 21:48:04 +0400

    Haas been added an icon and hidden a status bar

--- The Light project/ViewController.swift
+++ The Light project/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isLightOn = true
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+//    MARK - Method
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        
+        view.backgroundColor = isLightOn ? .white : .black
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isLightOn.toggle()
+        updateUI()
+    }
+    
 }
-

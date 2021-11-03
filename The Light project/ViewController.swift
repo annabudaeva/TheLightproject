01bff9302cf00e36d166c4fc222c0d5a32f9dc61
Anna <buanse@gmail.com>
2021-11-03 16:17:10 +0400

    Has been added  color changing from white through grey to black.

--- The Light project/ViewController.swift
+++ The Light project/ViewController.swift
@@ -2,34 +2,43 @@
 //  ViewController.swift
 //  The Light project
 //
-//  Created by Dmitry Kost on 01.11.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
     
-    var isLightOn = true
+    var lights = 0
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
     
-//    MARK - Method
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateUI()
-    }
-
-    fileprivate func updateUI() {
-        
-        view.backgroundColor = isLightOn ? .white : .black
+        switchlight(number: lights)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        isLightOn.toggle()
-        updateUI()
+        switchlight(number: lights)
+        
     }
     
+    //    MARK: Switch Light
+    func switchlight(number: Int) {
+        switch number {
+        case 1:
+            view.backgroundColor = .lightGray
+        case 2:
+            view.backgroundColor = .darkGray
+        case 3:
+            view.backgroundColor = .black
+        default:
+            view.backgroundColor = .white
+            lights = 0
+        }
+        
+        lights += 1
+        
+    }
 }

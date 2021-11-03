01df5c158ff7dd74c8b3b3e32821c8dfe4f0aef8
Anna <buanse@gmail.com>
2021-11-03 18:17:40 +0400

      Added a flashlight

--- The Light project/ViewController.swift
+++ The Light project/ViewController.swift
@@ -3,7 +3,7 @@
 //  The Light project
 //
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,14 +17,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         switchlight(number: lights)
+        updateView()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         switchlight(number: lights)
+        updateView()
         
     }
     
-    //    MARK: Switch Light
     func switchlight(number: Int) {
         switch number {
         case 1:
@@ -41,4 +42,29 @@ class ViewController: UIViewController {
         lights += 1
         
     }
+   
+// фонарик работает (включается на физическом устройстве), но не выключается. Не понимаю почему.
+    
+    func updateView() {
+           let device = AVCaptureDevice.default(for: AVMediaType.video)
+           
+           if let dev = device, dev.hasTorch {
+               view.backgroundColor = .black
+               do {
+                   try dev.lockForConfiguration()
+                   dev.torchMode = (lights != 0) ? .on : .off
+                   dev.unlockForConfiguration()
+               } catch {
+                   print(error)
+               }
+           } else {
+               
+               switchlight(number: lights)
+           }
+       }
+    
+    
 }
+
+
+
